06d7e775977b51c94b6956623f310d78ec1ac02b
davemins <davemins4125@naver.com>
2023-05-12 10:15:32 +0900

    2566

--- 07_2차원_배열/2566.py
+++ 07_2차원_배열/2566.py
@@ -1,18 +1,29 @@
 """최댓값"""
 
+# matrix = [list(map(int, input().split())) for _ in range(9)]
+
+# for row in matrix:
+#     print(' '.join(map(str, row)))
+
+# max_list = []
+
+# for i in matrix:
+#     max_list.append(max(i))
+
+# print(max(max_list))
+
+# max_row = max_list.index(max(max_list))
+# max_column = matrix[max_row].index(max(matrix[max_row]))
+
+# print(max_row + 1, max_column + 1)
+
 matrix = [list(map(int, input().split())) for _ in range(9)]
 
-for row in matrix:
-    print(' '.join(map(str, row)))
+max_value = max(map(max, matrix))
+print(max_value)
 
-max_list = []
-
-for i in matrix:
-    max_list.append(max(i))
-
-print(max(max_list))
-
-max_row = max_list.index(max(max_list))
-max_column = matrix[max_row].index(max(matrix[max_row]))
-
-print(max_row + 1, max_column + 1)
+for i in range(9):
+    for j in range(9):
+        if matrix[i][j] == max_value:
+            print(i + 1, j + 1)
+            break
