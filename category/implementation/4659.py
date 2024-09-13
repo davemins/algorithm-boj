a2259723bca3cf0542020ccbcf31508010427194
davemins <davemins4125@naver.com>
2024-09-13 11:44:55 +0900

    4659

--- category/implementation/4659.py
+++ category/implementation/4659.py
@@ -1,12 +1,12 @@
 '''
 # 문제 이해
-
+검사하여 조건이 맞는지 확인
 
 # 발상
-
+분기문으로 처리
 
 # 복잡도
-
+많음
 
 '''
 
@@ -17,36 +17,37 @@ aeiou = ['a', 'e', 'i', 'o', 'u']
 
 while data != 'end':
     if len(data) == 1:
-        if data == 'a' or data == 'e' or data == 'i' or data == 'o' or data == 'u':
-            print("<" + data + ">" " is acceptable.")
+        if data in aeiou:
+            print(f"<{data}> is acceptable.")
         else:
-            print("<" + data + ">" " is not acceptable.")
+            print(f"<{data}> is not acceptable.")
     elif len(data) == 2:
-        if data[0] == data[1] and data[0] != 'e' and data[0] != 'o':
-            print("<" + data + ">" " is not acceptable.")
-        elif 'a' not in data or 'e' not in data or 'i' not in data or 'o' not in data or 'u' in data:
-            print("<" + data + ">" " is not acceptable.")
+        if data[0] == data[1] and data[0] not in ['e', 'o']:
+            print(f"<{data}> is not acceptable.")
+        elif all(vowel not in data for vowel in aeiou):
+            print(f"<{data}> is not acceptable.")
         else:
-            print("<" + data + ">" " is acceptable.")
+            print(f"<{data}> is acceptable.")
     else:
-        if 'a' not in data and 'e' not in data and 'i' not in data and 'o' not in data and 'u' in data:
-            print("<" + data + ">" " is not acceptable.")
+        if all(vowel not in data for vowel in aeiou):
+            print(f"<{data}> is not acceptable.")
         else:
+            acceptable = True
             for i in range(1, len(data)):
-                if data[i] == data[i - 1] and data[i] != 'e' and data[i] != 'o':
-                    print("<" + data + ">" " is not acceptable.")
+                if data[i] == data[i - 1] and data[i] not in ['e', 'o']:
+                    acceptable = False
                     break
-                else:
-                    if i >= 2:
-                        if data[i - 2] in aeiou and data[i - 1] in aeiou and data[i] in aeiou:
-                            print("<" + data + ">" " is not acceptable.")
-                            break
-                        elif data[i - 2] not in aeiou and data[i - 1] not in aeiou and data[i] not in aeiou:
-                            print("<" + data + ">" " is not acceptable.")
-                            break
-                if i == len(data) - 1:
-                    print("<" + data + ">" " is acceptable.")
+                if i >= 2:
+                    if (data[i - 2] in aeiou and data[i - 1] in aeiou and data[i] in aeiou) or \
+                       (data[i - 2] not in aeiou and data[i - 1] not in aeiou and data[i] not in aeiou):
+                        acceptable = False
+                        break
+            if acceptable:
+                print(f"<{data}> is acceptable.")
+            else:
+                print(f"<{data}> is not acceptable.")
     data = input()
+
 
 
 '''
@@ -58,6 +59,7 @@ while data != 'end':
 
 # 느낀 점
 분기가 많을 수록 골이 아프다..
-
-
+acceptable을 만들 걸 그랬다.
+이번에 방법 하나 배워간다.
+하여간 구현을 잘 못 풀고 있는 게 맞다. ㅠㅠ
 '''
