62943d8779c2e97a9b7105a851f19fe6c95ccb3d
davemins <davemins4125@naver.com>
2024-09-13 14:22:56 +0900

    1213

--- category/implementation/1213.py
+++ category/implementation/1213.py
@@ -12,52 +12,37 @@
 str_input = input()
 
 str_list = []
-
 result_list = []
-
 odd = ''
+odd_count = 0  # 홀수 개수 문자의 수를 셀 변수
 
-
+# 문자열을 리스트에 저장
 for i in range(len(str_input)):
     str_list.append(str_input[i])
 
+# 문자의 개수를 세고 팰린드롬을 만들 수 있는지 확인
+for char in set(str_list):  # 중복을 피하기 위해 set 사용
+    count = str_list.count(char)
+    if count % 2 == 1:  # 홀수인 경우
+        odd_count += 1
+        odd = char  # 홀수인 문자를 odd에 저장 (최대 하나)
+        if odd_count > 1:  # 홀수인 문자가 2개 이상이면 팰린드롬 불가
+            print("I'm Sorry Hansoo")
+            exit()
+    result_list.extend([char] * (count // 2))  # 절반만큼 추가
 
-for i in range(len(str_list)):
-    if str_list[i] not in result_list:
-        count = str_list.count(str_list[i])
-        if count == 1:
-            if odd != '':
-                odd = str_list[i]
-            else:
-                odd += str_list[i]
-        if count % 2 == 0:
-            for j in range(count // 2):
-                result_list.append(str_list[i])
-        else:
-            for j in range(count // 2):
-                result_list.append(str_list[i])
-                if odd != '':
-                    odd = str_list[i]
-                else:
-                    odd += str_list[i]
+# 팰린드롬을 만들 수 있는 경우
+result_list.sort()  # 팰린드롬의 첫 번째 절반 정렬
+result = ''.join(result_list)  # 첫 번째 절반 문자열
 
-result_list.sort()
-result = ''
+# 팰린드롬의 완성을 위해 중앙에 odd 문자를 넣고, 뒤집은 절반 추가
+if odd_count == 1:
+    result += odd  # 가운데에 홀수 문자를 추가
 
-for i in result_list:
-    result += i
+result += ''.join(reversed(result_list))  # 뒤집은 절반 추가
 
-if len(odd) == 0:
-    for i in range(len(result_list) - 1, -1, -1):
-        result += result_list[i]
-    print(result)
-elif len(odd) == 1:
-    result += odd
-    for i in range(len(result_list) - 1, -1, -1):
-        result += result_list[i]
-    print(result)
-else:
-    print("I'm Sorry Hansoo")
+print(result)
+
 '''
 # 푼 시간
 33분
@@ -68,4 +53,6 @@ else:
 # 느낀 점
 뭔가 되게 바쁘게 풀긴 했는데 왜 틀린지 모르겠다.
 발상은 좋았는데 디테일을 자꾸 놓치는 것 같다.
+
+join 배워간다.
 '''
