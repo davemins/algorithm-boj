ab08db90dd369ab7b9135c4e5f455b1ebdd8ed34
davemins <davemins4125@naver.com>
2024-09-16 09:50:48 +0900

    2606 정답

--- category/dfs, bfs/2606.py
+++ category/dfs, bfs/2606.py
@@ -9,31 +9,34 @@
 
 
 '''
-computer = int(input())
-link = int(input())
+computer = int(input())  # 컴퓨터 수
+link = int(input())  # 연결된 링크 수
 
-information = []
-computers = [[] for i in range(computer)]
+# 각 컴퓨터에 연결된 컴퓨터 정보를 저장할 리스트
+computers = [[] for _ in range(computer)]
 
-for i in range(link):
-    information.append(list(map(int, input().split())))
+# 연결 정보 입력받기
+for _ in range(link):
+    a, b = map(int, input().split())
+    computers[a - 1].append(b - 1)
+    computers[b - 1].append(a - 1)  # 양방향 연결
 
-for j in information:
-    computers[j[0] - 1].append(j)
-
-def dfs(data, x, y, count):
-    if data[x]:
-        nx, ny = data[x].pop()
-        count += 1
-        count += dfs(data, nx, ny, count)
+# DFS 함수
+def dfs(computers, visited, x):
+    visited[x] = True
+    count = 1  # 현재 컴퓨터도 감염되므로 count = 1로 시작
+    for neighbor in computers[x]:
+        if not visited[neighbor]:
+            count += dfs(computers, visited, neighbor)
     return count
 
-count = 0
+# 방문 여부 체크 리스트
+visited = [False] * computer
 
-count += dfs(computers, 0, 0, count)
+# 1번 컴퓨터(0번 인덱스)에서 시작
+result = dfs(computers, visited, 0) - 1  # 1번 컴퓨터는 제외하고 세기 위해 -1
 
-result = count
-print(count)
+print(result)
 '''
 # 푼 시간
 30분
