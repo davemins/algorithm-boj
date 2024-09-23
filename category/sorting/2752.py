cd7150d81a3d8d5bdd61f5cdb11fa5ddab32f5ed
davemins <davemins4125@naver.com>
2024-09-23 21:20:52 +0900

    2752

---
+++ category/sorting/2752.py
@@ -0,0 +1,29 @@
+'''
+# 문제 이해
+오름차순 정렬해보자.
+
+# 발상
+정렬
+
+# 복잡도
+O(1)의 시간복잡도를 가진다.
+
+'''
+numbers = list(map(int, input().split()))
+
+numbers.sort()
+
+for i in numbers:
+    print(i, end=' ')
+
+
+'''
+# 푼 시간
+2분
+
+# 채점
+정답
+
+# 느낀 점
+정렬도 재밌게 공부해보자.
+'''
