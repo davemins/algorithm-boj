fc152fba784b7f2a7835884fd1458296019484d3
davemins <davemins4125@naver.com>
2024-09-30 22:42:33 +0900

    28235

---
+++ category/implementation/28235.py
@@ -0,0 +1,34 @@
+'''
+# 문제 이해
+구호 SONGDO에 대해 HIGHSCHOOL로 응원.
+구호 CODE에 대해 MASTER로 응원.
+구호 2023에 대해 0611로 응원.
+구호 ALGORITHM에 대해 CONTEST로 응원.
+
+# 발상
+구현
+
+# 복잡도
+
+
+'''
+data = input()
+
+if data == 'SONGDO':
+    print('HIGHSCHOOL')
+elif data == 'CODE':
+    print('MASTER')
+elif data == '2023':
+    print('0611')
+elif data == 'ALGORITHM':
+    print('CONTEST')
+'''
+# 푼 시간
+3분 30초
+
+# 채점
+정답
+
+# 느낀 점
+귀찮음
+'''
